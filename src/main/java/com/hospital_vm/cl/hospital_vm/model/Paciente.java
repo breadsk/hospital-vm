aa23d9a8e9b2bd5bec62fd070499b9879c6fb7e7
Nicolas <nicolas.programador@gmail.com>
2025-03-02 18:09:47 -0300

    Modelo y mapeo con anotaciones JPA ok

---
+++ src/main/java/com/hospital_vm/cl/hospital_vm/model/Paciente.java
@@ -0,0 +1,34 @@
+package com.hospital_vm.cl.hospital_vm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name= "paciente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paciente {
+    @Id // Le indico con el decorador que este sera el id de mi tabla
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Con esto le digo que sera autoincrementable
+    private Integer id;
+
+    @Column(unique=true, length = 13, nullable=false)//Con esto le digo que solo es un dato de columna
+    private String run;
+
+    @Column(nullable=false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = true)
+    private Date fechaNacimiento;
+
+    @Column(nullable=false)
+    private String correo;
+}
